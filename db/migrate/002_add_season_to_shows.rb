4748cd592bf771c71e36b520f50b930510e30d9a
angiemalaika <malaika.mumba@gmail.com>
2019-06-28 15:20:51 -0400

    Done.

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,6 +1,7 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
    add_column :shows, :season, :string
-   #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
     end
 end
+
+ #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
--- models/app/show.rb
+++ models/app/show.rb
@@ -1,4 +1,5 @@
 class Show < ActiveRecord::Base
+
     def self.highest_rating
       self.maximum(:rating)
     end
@@ -26,4 +27,4 @@ class Show < ActiveRecord::Base
     def self.shows_by_alphabetical_order
       self.order(:name)
     end
-end 
+  end
